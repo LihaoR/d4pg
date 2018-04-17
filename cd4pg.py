b7d5d82a40fe7c7d2b04e20941637657cecb5af8
LihaoR <32670993+LihaoR@users.noreply.github.com>
2018-04-17 17:04:15 +0800

    much slower than the other one

--- cd4pg.py
+++ cd4pg.py
@@ -22,9 +22,6 @@ def update_target_graph(from_scope,to_scope):
         op_holder.append(to_var.assign(from_var))
     return op_holder
 
-def discount(x, gamma):
-    return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
-
 class AC_Network():
     def __init__(self,s_size,a_size,scope,atrainer,ctrainer):
         with tf.variable_scope(scope):
